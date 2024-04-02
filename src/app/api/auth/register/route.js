8dfcd85dd9aa71e81f9a124f0a575512b93b6ac7
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-02 17:59:05 -0500

    feat(data): Saving data to database

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,11 +1,42 @@
 import { NextResponse } from "next/server";
+import db from "@/utils/prisma";
+import bcrypt from "bcrypt";
 
 export async function POST(request) {
   try {
-    const data = await request.json()
-    console.log(data);
-    return NextResponse.json('OK')
+    const data = await request.json();
+    let errors = []
+
+    const userFound = await db.user.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+    if (userFound) errors.push("Username already exists")
+    const emailFound = await db.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+    if (emailFound) errors.push("Email is already in use")
+    
+    // Sending errors array
+    if (userFound || emailFound) return NextResponse.json(errors, { status: 409 })
+    
+
+    const passwordHash = await bcrypt.hash(data.password, 10);
+    const newUser = await db.user.create({
+      data: {
+        username: data.username,
+        email: data.email,
+        password: passwordHash,
+      },
+    });
+
+    const { password: _, ...user } = newUser;
+
+    return NextResponse.json({ data: user, message: "Admin created!" });
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 })
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
