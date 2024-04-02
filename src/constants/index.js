f8310bce32745c043c7cb09c7ea3cfb5099a6cef
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-02 16:43:13 -0500

    feat(fields): Adding fields for mapping

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,43 @@
-import { HomeIcon, UserGroupIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, UserGroupIcon } from "@heroicons/react/24/outline";
 
 const navLinks = [
   { name: "Dashboard", to: "/admin", icon: HomeIcon },
-  { name: "Admins", to: "/admin/admins", icon: UserGroupIcon }
-]
+  { name: "Admins", to: "/admin/admins", icon: UserGroupIcon },
+];
 
-export { navLinks }
+const registerFields = [
+  {
+    label: "username",
+    title: "Username",
+    type: "text",
+    placeholder: "zorg",
+  },
+  {
+    label: "email",
+    title: "Email",
+    type: "text",
+    placeholder: "email@example.com",
+  },
+  {
+    label: "password",
+    title: "Password",
+    type: "password",
+    placeholder: "*****",
+  },
+  {
+    label: "confim_password",
+    title: "Confirm password",
+    type: "password",
+    placeholder: "*****",
+  },
+];
+const productFields = [
+  {
+    label: "product",
+    title: "Product",
+    type: "text",
+    placeholder: "Camera",
+  },
+];
+
+export { navLinks, registerFields, productFields };
