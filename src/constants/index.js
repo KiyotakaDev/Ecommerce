0b1622d6f5986a7a980a0823166cecc269ac1165
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-02 15:04:26 -0500

    feat(link): Adding dashboard

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
-import { UserGroupIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, UserGroupIcon } from '@heroicons/react/24/outline'
 
 const navLinks = [
+  { name: "Dashboard", to: "/admin", icon: HomeIcon },
   { name: "Admins", to: "/admin/admins", icon: UserGroupIcon }
 ]
 
