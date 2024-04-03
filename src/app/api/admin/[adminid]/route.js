075e5381d1338649c8f4f42988a9440a553b5c1f
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-02 19:14:30 -0500

    feat(delete): Delete admin functionality is added

---
+++ src/app/api/admin/[adminid]/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import db from '@/utils/prisma'
+
+export async function DELETE(_, { params }) {
+  try {
+    const { adminid } = params;
+    await db.user.delete({
+      where: {
+        id: parseInt(adminid)
+      }
+    })
+    return NextResponse.json("Admin deleted!")
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
