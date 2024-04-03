4cb1db4ab1605c7df59a26ec3030091e384c7fc2
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-02 19:15:26 -0500

    feat(layout): Layout is used for admins

--- src/app/admin/admins/page.jsx
+++ src/app/admin/admins/page.jsx
@@ -1,12 +1,34 @@
-import Link from 'next/link'
+'use client'
+
+import DataLayout from '@/components/DataLayout'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const AdminsPage = () => {
+  const [admins, setAdmins] = useState([])
+
+  useEffect(() => {
+    const getAdmins = async () => {
+      try {
+        const response = await axios.get('/api/admins')
+        setAdmins(response.data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getAdmins()
+  }, [])
+  
   return (
-    <div>
-      <Link href="admins/new" className='app-btn'>
-        Add admin
-      </Link>
-    </div>
+    <>
+      <DataLayout dataProps={{
+        pLink: 'admins/new', 
+        pTitle: 'Admins', 
+        pField: 'Admin', 
+        pMapper: admins,
+        setField: setAdmins
+      }} />
+    </>
   )
 }
 
