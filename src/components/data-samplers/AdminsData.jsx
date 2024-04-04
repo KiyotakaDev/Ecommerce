90ce71b70be67ab0030fa80704aea87730b3972a
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-04 14:49:07 -0500

    feat(actions): Button is added and also the email field

--- src/components/data-samplers/AdminsData.jsx
+++ src/components/data-samplers/AdminsData.jsx
@@ -1,4 +1,4 @@
-import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { InformationCircleIcon, PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 
 const AdminsData = (props) => {
@@ -11,39 +11,52 @@ const AdminsData = (props) => {
           <thead>
             <tr>
               <th>{props.pTitle}</th>
+              <th>email</th>
               <th>Actions</th>
             </tr>
           </thead>
           <tbody>
             {props.pMapper.map((field) => {
-              const { id, username, name } = field;
+              const { id, username, email, name } = field;
               const validation = props.pField === "Admin" ? username : name
               return (
                 <tr key={id}>
                   <td className="text-xl">
                     {validation}
                   </td>
+                  <td>{email}</td>
                   <td className="flex justify-center items-center gap-4">
-                    <button
-                      onClick={() => {
-                        props.deleteOnClick(id)
+                    {username !== 'root' ? (
+                      <>
+                      <button
+                        onClick={() => {
+                          props.handleAction(id)
+                          props.setSelected(validation)
+                        }}
+                        className="bg-red-400/80 action-btn"
+                      >
+                        <TrashIcon className="w-8 h-8" />
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => {
+                          props.handleAction(id)
+                          props.setSelected(validation)
+                        }}
+                        className="bg-yellow-400/80 action-btn"
+                      >
+                        <PencilSquareIcon className="w-8 h-8" />
+                        Edit
+                      </button>
+                      </>
+                    ) : (
+                      <button onClick={() => {
+                        props.handleAction(id)
                         props.setSelected(validation)
-                      }}
-                      className="bg-red-400/80 delete-handler-btn"
-                    >
-                      <TrashIcon className="w-8 h-8" />
-                      Delete
-                    </button>
-                    <button
-                      onClick={() => {
-                        props.deleteOnClick(id)
-                        props.setSelected(validation)
-                      }}
-                      className="bg-yellow-400/80 delete-handler-btn"
-                    >
-                      <PencilSquareIcon className="w-8 h-8" />
-                      Edit
-                    </button>
+                      }} className="bg-gray-300 action-btn w-full">
+                        <InformationCircleIcon className="w-6 h-6" />
+                      </button>
+                    )}
                   </td>
                 </tr>
               );
