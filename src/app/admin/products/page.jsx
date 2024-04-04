721a424b00bd01d514085b879aa96ef791290f80
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-03 19:37:11 -0500

    feat(get all): Get all functionality is used

--- src/app/admin/products/page.jsx
+++ src/app/admin/products/page.jsx
@@ -1,13 +1,34 @@
-import Link from 'next/link'
-import React from 'react'
+'use client'
+
+import DataLayout from '@/components/DataLayout'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Products = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const response = await axios.get('/api/products')
+        setProducts(response.data)
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getProducts()
+  }, [])
+  
   return (
-    <div>
-      <Link href={'products/new'} className="app-btn">
-        Add Products
-      </Link>
-    </div>
+    <>
+      <DataLayout dataProps={{
+        pLink: 'products/new', 
+        pTitle: 'Products', 
+        pField: 'Product', 
+        pMapper: products,
+        setMapper: setProducts
+      }} />
+    </>
   )
 }
 
