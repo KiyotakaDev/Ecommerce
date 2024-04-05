8e0627159283907ac6c03aef70cfe3294c10520a
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-04 23:42:59 -0500

    feat(upgrading): Improving back validations

--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -1,18 +1,27 @@
 import { z } from "zod";
 
 // Product validations
-const fileSchema = z.instanceof(File, { message: "At least 1 image" });
-const imageSchema = fileSchema.refine(
-  (file) => file.size === 0 || file.type.startsWith("image/")
-);
+const maxFileSize = 1024 * 1024 * 5; // 5MB in Bits
+const imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+
+const imageSchema = z
+  .instanceof(File, { message: "Each file must be an image" })
+  .refine((file) => file.size <= maxFileSize, {
+    message: "Max size allowed for each image is 5MB",
+  })
+  .refine((file) => imageTypes.includes(file.type), {
+    message: "Only .jpeg, .jpg and .png images are allowed",
+  });
 
 export const zodProduct = z.object({
   product: z
     .string()
     .min(1, { message: "Product must contain at least 1 character" }),
   images: z
-    .array(imageSchema.refine((file) => file.size > 0, "Image is required"))
-    .max(4, { message: "Maximum of 4 images allowed" }),
+    .array(imageSchema)
+    .refine((files) => files.length > 0, {
+      message: "At least 1 image is required",
+    }),
   description: z
     .string()
     .min(1, { message: "Description must conatin at least 1 character" }),
