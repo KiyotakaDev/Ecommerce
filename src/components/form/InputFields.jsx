4bdce00911661c8c04bf72e9834aa2f204a48684
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-04 20:32:30 -0500

    feat(preview): Adding image preview to the image upload

--- src/components/form/InputFields.jsx
+++ src/components/form/InputFields.jsx
@@ -1,6 +1,62 @@
+import { ArrowUpOnSquareIcon } from "@heroicons/react/24/outline";
 import React from "react";
+import { toast } from "react-toastify";
 
 const InputFields = (props) => {
+  const [images, setImages] = props.useState([]);
+  const CustomInput = props.html;
+
+  if (props.type === "file") {
+    const uploadImages = (e) => {
+      const files = Array.from(e.target?.files)
+      if (files.length > 4 || images > 4) {
+        toast.error("Max 4 images")
+        return
+      }
+
+      setImages(prev => [...prev, ...files])
+    }
+
+    return (
+      <>
+        <label
+          htmlFor={props.label}
+          className="text-teal-900 font-semibold text-lg"
+        >
+          {props.title}
+        </label>
+        {props.type === "file" ? (
+          <div className="flex">
+            {images.length > 0 ? (
+              <>
+                {images.map((img, i) => (
+                  <img
+                    key={i}
+                    src={URL.createObjectURL(img)}
+                    alt={`Preview-${i}`}
+                    className="h-24 w-auto rounded-lg"
+                  />
+                ))}
+              </>
+            ) : null}
+            <label className="w-24 h-24 flex flex-col justify-center items-center bg-gray-300 rounded-lg cursor-pointer hover:scale-105 transition-all duration-200 ease-out">
+              <ArrowUpOnSquareIcon className="stroke-teal-700 w-12 h-12" />
+              <p className="text-teal-700">Upload</p>
+              <input
+                type={props.type}
+                name={props.label}
+                multiple
+                accept="image/*"
+                className="hidden"
+                onChange={uploadImages}
+              />
+            </label>
+          </div>
+        ) : null}
+      </>
+    );
+  }
+
   return (
     <>
       <label
@@ -9,13 +65,10 @@ const InputFields = (props) => {
       >
         {props.title}
       </label>
-      <props.html
+      <CustomInput
         type={props.type}
         name={props.label}
         placeholder={props.placeholder}
-        {...(props.type === "file"
-          ? { multiple: true, accept: "image/*" }
-          : {})}
         {...(props.type === "number"
           ? { onChange: (e) => props.setPrice(e.target.value) }
           : {})}
