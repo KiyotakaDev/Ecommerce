31feeca6896c34288a307367298cb98a57742498
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-05 15:55:29 -0500

    feat(zustand): Using state manager

--- src/app/admin/admins/page.jsx
+++ src/app/admin/admins/page.jsx
@@ -3,31 +3,33 @@
 import Animation from '@/components/Animation'
 import DataLayout from '@/components/data-samplers/DataLayout'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import { useDataStore } from '@/store/store'
 
 const AdminsPage = () => {
-  const [admins, setAdmins] = useState([])
+  const { setData, setMapper } = useDataStore()
 
   useEffect(() => {
     const getAdmins = async () => {
       try {
         const response = await axios.get('/api/admins')
-        setAdmins(response.data)
+        const admins = response.data
+        const dataToStore = {
+          link: "admins/new",
+          field: "Admin"
+        }
+        setData(dataToStore)
+        setMapper(admins)
       } catch (error) {
         console.log(error);
       }
     }
     getAdmins()
-  }, [])
+  }, [setData])
   
   return (
     <Animation>
-      <DataLayout dataProps={{
-        pLink: 'admins/new', 
-        pField: 'Admin', 
-        pMapper: admins,
-        setMapper: setAdmins
-      }} />
+      <DataLayout />
     </Animation>
   )
 }
--- src/app/admin/products/page.jsx
+++ src/app/admin/products/page.jsx
@@ -1,35 +1,37 @@
-'use client'
+"use client";
 
-import Animation from '@/components/Animation'
-import DataLayout from '@/components/data-samplers/DataLayout'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import Animation from "@/components/Animation";
+import DataLayout from "@/components/data-samplers/DataLayout";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDataStore } from "@/store/store";
 
 const Products = () => {
-  const [products, setProducts] = useState([])
+  const { setData, setMapper } = useDataStore();
 
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const response = await axios.get('/api/products')
-        setProducts(response.data)
+        const response = await axios.get("/api/products");
+        const products = response.data
+        const dataToStore = {
+          link: "products/new",
+          field: "Product"
+        }
+        setMapper(products)
+        setData(dataToStore);
       } catch (error) {
         console.log(error.message);
       }
-    }
-    getProducts()
-  }, [])
-  
+    };
+    getProducts();
+  }, [setData]);
+
   return (
     <Animation>
-      <DataLayout dataProps={{
-        pLink: 'products/new', 
-        pField: 'Product', 
-        pMapper: products,
-        setMapper: setProducts
-      }} />
+      <DataLayout />
     </Animation>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
