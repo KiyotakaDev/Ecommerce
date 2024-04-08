2c0284a12a664b0ab7ae12782e24bf52d03c5afa
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-08 16:12:48 -0500

    feat(reset): Values reset functionality is added

--- src/store/inputStore.js
+++ src/store/inputStore.js
@@ -4,17 +4,28 @@ export const useInputStore = create((set) => ({
   // Initial state
   productFormData: {
     product: "",
-    images: [],
+    imagesPath: [],
     description: "",
     price: 0,
   },
   images: [],
   isLoading: false,
+  onMainPage: false,
 
   // Functions
-  setProductFormData: (data) =>
-    set((state) => ({ productFormData: { ...state.productFormData, ...data } })),
+  setProductFormData: (data) => 
+      set((state) => ({ productFormData: { ...state.productFormData, ...data } })),
+  resetProduct: () => set({
+    productFormData: {
+      product: "",
+      imagesPath: [],
+      description: "",
+      price: 0
+    },
+    images: []
+  }),
   setIsLoading: (bool) => set({ isLoading: bool }),
   setImages: (image) =>
     set((state) => ({ images: [...state.images, ...image] })),
+  setOnMainPage: (bool) => set({ onMainPage: bool })
 }));
