4bb79cdc535d461ace6c58fb750091464faa5ea0
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-07 22:16:29 -0500

    feat(endpoints): Adding endpoints

--- src/app/api/product/[productid]/route.js
+++ src/app/api/product/[productid]/route.js
@@ -14,3 +14,31 @@ export async function DELETE(_, { params }) {
     return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
+
+export async function GET(_, { params }) {
+  try {
+    const { productid } = params;
+    const product = await db.product.findFirst({
+      where: {
+        id: parseInt(productid)
+      }
+    })
+    return NextResponse.json({ data: product, message: "Product found" })
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
+
+export async function POST(_, { params }) {
+  try {
+    const { productid } = params;
+    await db.product.update({
+      where: {
+        id: parseInt(productid)
+      }
+    })
+    return NextResponse.json("Product updated!")
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
