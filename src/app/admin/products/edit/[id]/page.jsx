ba9395d739c36529a2a346c3f92ed3602ad5bd31
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-08 17:22:50 -0500

    feat(params): Giving id as params instead of useParams

--- src/app/admin/products/edit/[id]/page.jsx
+++ src/app/admin/products/edit/[id]/page.jsx
@@ -3,14 +3,14 @@
 import Animation from "@/components/Animation";
 import FormLayout from "@/components/form/FormLayout";
 import { addProductFields } from "@/constants";
-import { useParams } from "next/navigation";
 import { useEffect } from "react";
 import axios from "axios";
 import MainLoader from "@/components/loaders/MainLoader";
 import { useInputStore } from "@/store/inputStore";
+import { useDataStore } from '@/store/dataStore'
 
 const AddProduct = () => {
-  const { id } = useParams();
+  const { id } = useDataStore();
   const { setProductFormData, isLoading, setIsLoading } = useInputStore()
   
   useEffect(() => {
