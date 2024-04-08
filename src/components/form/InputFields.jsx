d8123ef866a0bc671965367c2e8e629ffd11852d
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-08 16:11:42 -0500

    feat(onChange): Values and on change handler are used

--- src/components/form/InputFields.jsx
+++ src/components/form/InputFields.jsx
@@ -3,8 +3,9 @@ import { toast } from "react-toastify";
 import { ArrowUpOnSquareIcon } from "@heroicons/react/24/outline";
 
 const InputFields = (props) => {
-  const { html: CustomTag, label, title, type, placeholder } = props;
-  const { images, setImages, setProductFormData, productFormData } = useInputStore();
+  const { html: CustomTag, label, title, type, placeholder, editing } = props;
+  const { setImages, images, setProductFormData, productFormData } =
+    useInputStore();
 
   const maxFileSize = 1024 * 1024 * 5; // 5MB in Bits
   const imageTypes = ["image/jpeg", "image/jpg", "image/png"];
@@ -33,7 +34,7 @@ const InputFields = (props) => {
       toast.error("Image is required");
       return;
     }
-    if (files.length > 4 || images.length > 3) {
+    if (files.length > 4 || productFormData.imagesPath.length > 3) {
       toast.error("Max 4 images");
       return;
     }
@@ -41,10 +42,22 @@ const InputFields = (props) => {
   };
 
   const handleFormInputs = (e) => {
-    const { value } = e.target
-    setProductFormData({ [label]: value })
-    console.log(productFormData);
-  }
+    const { value } = e.target;
+    setProductFormData({ [label]: value });
+  };
+
+  const renderImagesPreview = () => {
+    const dbImages = productFormData.imagesPath;
+    const imagesToRender = dbImages.length > 0 ? dbImages : images;
+    return imagesToRender.map((image, index) => (
+      <img
+        key={index}
+        src={dbImages.length > 0 ? image : URL.createObjectURL(image)}
+        alt={`Preview-${index}`}
+        className="h-24 w-auto rounded-lg"
+      />
+    ));
+  };
 
   if (type === "file") {
     return (
@@ -53,18 +66,7 @@ const InputFields = (props) => {
           {title}
         </label>
         <div className="flex gap-2">
-          {images.length > 0 ? (
-            <>
-              {images.map((img, i) => (
-                <img
-                  key={i}
-                  src={URL.createObjectURL(img)}
-                  alt={`Preview-${i}`}
-                  className="h-24 w-auto rounded-lg"
-                />
-              ))}
-            </>
-          ) : null}
+          {renderImagesPreview()}
           <label className="w-24 h-24 flex flex-col justify-center items-center bg-gray-300 rounded-lg cursor-pointer hover:scale-105 transition-all duration-200 ease-out">
             <ArrowUpOnSquareIcon className="stroke-teal-700 w-12 h-12" />
             <p className="text-teal-700">Upload</p>
@@ -91,15 +93,17 @@ const InputFields = (props) => {
         type={type}
         name={label}
         placeholder={placeholder}
-        {...(productFormData ? { value: productFormData[label] } : {})}
+        {...(productFormData && editing
+          ? { value: productFormData[label] }
+          : {})}
         onChange={(e) => handleFormInputs(e)}
         className="input-fields"
       />
-      {type == "number" ? (
+      {type === "number" && (
         <span className="-mt-2 font-semibold text-teal-700/60">
           ${productFormData.price / 100}
         </span>
-      ) : null}
+      )}
     </div>
   );
 };
