245fe966e68d04cf533d356b427b703b22e07de2
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-07 22:15:46 -0500

    feat(product): Getting product data

--- src/app/admin/products/edit/[id]/page.jsx
+++ src/app/admin/products/edit/[id]/page.jsx
@@ -1,18 +1,50 @@
+"use client";
+
 import Animation from "@/components/Animation";
 import FormLayout from "@/components/form/FormLayout";
 import { addProductFields } from "@/constants";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MainLoader from "@/components/loaders/MainLoader";
 
 const AddProduct = () => {
+  const { id } = useParams();
+
+  const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get(`/api/product/${id}`);
+        const data = response.data.data;
+        setProduct(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getProduct();
+  }, []);
+
   return (
-    <Animation>
-      <FormLayout
-        formProps={{
-          id: "products",
-          pTitle: "Edit Product",
-          pMapper: addProductFields,
-        }}
-      />
-    </Animation>
+    <>
+      {isLoading ? (
+        <MainLoader />
+      ) : (
+        <Animation>
+          <FormLayout
+            formProps={{
+              id: "products",
+              pTitle: "Edit Product",
+              pMapper: addProductFields,
+              pData: product,
+            }}
+          />
+        </Animation>
+      )}
+    </>
   );
 };
 
