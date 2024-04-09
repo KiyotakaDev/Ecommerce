ad0441d815cf7e08109930bf63314a46dcc0e9d7
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-09 18:35:05 -0500

    feat(update): category

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -13,3 +13,32 @@ export async function GET() {
     return NextResponse.json({ error: error.message }, { status: 500 })
   }
 }
+
+export async function PUT(request) {
+  try {
+    const data = await request.json();
+    const category = data.category.trim();
+    const parent = data.parent.trim();
+    const id = data.id
+
+    if (!category)
+      return NextResponse.json(
+        { errors: ["Category can not be empty"] },
+        { status: 400 }
+      );
+
+    const newCategory = await db.category.update({
+      where: { 
+        id: id
+      },
+      data: {
+        name: category,
+        parent: parseInt(parent) 
+      }
+    })
+
+    return NextResponse.json({ data: newCategory, message: "New category created!" })
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
