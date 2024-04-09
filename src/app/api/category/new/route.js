70e1b7e68600e33faf33702a1541d60a9e5b5963
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-09 16:20:45 -0500

    feat(parent): Field is added

--- src/app/api/category/new/route.js
+++ src/app/api/category/new/route.js
@@ -1,25 +1,31 @@
 import { NextResponse } from "next/server";
-import db from '@/utils/prisma'
+import db from "@/utils/prisma";
 
 export async function POST(request) {
   try {
-    const data = await request.json()
-    const category = data.category.trim()
-    if (!category) return NextResponse.json({ errors: ["Category can not be empty"] }, { status: 400 })
+    const data = await request.json();
+    const category = data.category.trim();
+    const parent = data.parent.trim();
+
+    if (!category)
+      return NextResponse.json(
+        { errors: ["Category can not be empty"] },
+        { status: 400 }
+      );
 
     const categoryFound = await db.category.findUnique({
       where: {
         name: category
       }
     })
-
     if (categoryFound) {
       return NextResponse.json({ errors: ["Category already exists"] }, { status: 409 })
     }
 
     const newCategory = await db.category.create({
       data: {
-        name: category
+        name: category,
+        parentCategory: parent 
       }
     })
 
