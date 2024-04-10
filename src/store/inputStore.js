82a2facbfbfcf13bf497275b0a615757d5f1bb67
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-10 01:15:53 -0500

    feat(store): Is added

--- src/store/inputStore.js
+++ src/store/inputStore.js
@@ -2,30 +2,9 @@ import { create } from "zustand";
 
 export const useInputStore = create((set) => ({
   // Initial state
-  productFormData: {
-    product: "",
-    imagesPath: [],
-    description: "",
-    price: 0,
-  },
-  images: [],
-  isLoading: false,
-  onMainPage: false,
+  productInfo: [],
 
   // Functions
-  setProductFormData: (data) => 
-      set((state) => ({ productFormData: { ...state.productFormData, ...data } })),
-  resetProduct: () => set({
-    productFormData: {
-      product: "",
-      imagesPath: [],
-      description: "",
-      price: 0
-    },
-    images: []
-  }),
-  setIsLoading: (bool) => set({ isLoading: bool }),
-  setImages: (image) =>
-    set((state) => ({ images: [...state.images, ...image] })),
-  setOnMainPage: (bool) => set({ onMainPage: bool })
+  setProductInfo: (data) => set({ productInfo: data }),
+  resetProduct: () => set({ productInfo: []})
 }));
