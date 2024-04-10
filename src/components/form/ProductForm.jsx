8234c8f0efa70da2a0d04df7e61a5c34c4a28d7f
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-09 23:47:39 -0500

    feat(imageUpload): And preview images are successfully working

--- src/components/form/ProductForm.jsx
+++ src/components/form/ProductForm.jsx
@@ -5,18 +5,22 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { ArrowUpOnSquareIcon } from "@heroicons/react/24/outline";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ProductForm = ({
   id,
   product: existingProduct,
+  imagesPath: existingImages,
   description: existingDescription,
   price: existingPrice,
 }) => {
   const router = useRouter();
+  const [files, setFiles] = useState([]);
 
   const initialState = {
     product: existingProduct || "",
-    images: [],
+    imagesPath: [] || existingImages,
     description: existingDescription || "",
     price: existingPrice || 0,
   };
@@ -45,9 +49,25 @@ const ProductForm = ({
     const { value } = e.target;
     setProductData((prev) => ({ ...prev, [label]: value }));
   };
-  const handleImage = (e) => {
-    const { files } = e.target;
-    setProductData((prev) => ({ ...prev, images: [...files] }));
+
+  const handleImage = async (e) => {
+    try {
+      const files = e.target?.files;
+      if (files?.length > 0) {
+        const imageData = new FormData();
+        for (const image of files) {
+          imageData.append("images", image);
+        }
+        const response = await axios.post("/api/upload", imageData);
+        const path = response.data.imagesPath;
+        setProductData((prev) => ({
+          ...prev,
+          imagesPath: [...prev.imagesPath, ...path],
+        }));
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
@@ -67,17 +87,27 @@ const ProductForm = ({
             {label == "product" ? (
               <div className="flex flex-col">
                 <label className="form-label">Images</label>
-                <label className="w-24 h-24 flex flex-col justify-center items-center bg-gray-300 rounded-lg cursor-pointer hover:scale-105 transition-all duration-200 ease-out">
-                  <ArrowUpOnSquareIcon className="stroke-teal-700 w-12 h-12" />
-                  <p className="text-teal-700">Upload</p>
-                  <input
-                    type="file"
-                    accept="image/png, image/jpg, image/jpeg"
-                    multiple
-                    onChange={handleImage}
-                    className="hidden"
-                  />
-                </label>
+                <div className="flex gap-2">
+                  {!!productData.imagesPath?.length &&
+                    productData.imagesPath.map((image, index) => (
+                      <img
+                        src={image}
+                        alt={`Preview-${index}`}
+                        className="h-24 w-auto rounded-lg"
+                      />
+                    ))}
+                  <label className="w-24 h-24 flex flex-col justify-center items-center bg-gray-300 rounded-lg cursor-pointer hover:scale-105 transition-all duration-200 ease-out">
+                    <ArrowUpOnSquareIcon className="stroke-teal-700 w-12 h-12" />
+                    <p className="text-teal-700">Upload</p>
+                    <input
+                      type="file"
+                      accept="image/png, image/jpg, image/jpeg"
+                      multiple
+                      onChange={handleImage}
+                      className="hidden"
+                    />
+                  </label>
+                </div>
               </div>
             ) : null}
             {type === "number" && (
@@ -91,6 +121,7 @@ const ProductForm = ({
       <button type="submit" className="app-btn mt-3">
         Save
       </button>
+      <ToastContainer />
     </form>
   );
 };
