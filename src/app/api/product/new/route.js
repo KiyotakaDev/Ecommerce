eb8d2bae48eec84d3ea22fdc2800732943ff949e
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-09 23:50:15 -0500

    feat(create): Product

---
+++ src/app/api/product/new/route.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server'
+
+export async function POST(request) {
+  try {
+    const data = await request.json()
+    console.log(data);
+
+    return NextResponse.json('ok')
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
