65db949345bd24706c6f0704b2c46534f3b538a7
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-11 14:07:30 -0500

    feat(nav): For client side

--- src/components/client/Header.jsx
+++ src/components/client/Header.jsx
@@ -2,15 +2,17 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <header>
-      <Link href={'/'}>Ecommerce</Link>
-      <nav>
-        <Link href={'/'}>Home</Link>
-        <Link href={'/products'}>All products</Link>
-        <Link href={'/categories'}>Categories</Link>
-        <Link href={'/account'}>Account</Link>
-        <Link href={'/cart'}>Cart (0)</Link>
-      </nav>
+    <header className='bg-zinc-900 text-2xl'>
+      <div className='wrapper flex justify-between py-6'>
+        <Link href={'/'} className='text-white'>Ecommerce</Link>
+        <nav className='text-zinc-300 flex gap-6'>
+          <Link href={'/'}>Home</Link>
+          <Link href={'/products'}>All products</Link>
+          <Link href={'/categories'}>Categories</Link>
+          <Link href={'/account'}>Account</Link>
+          <Link href={'/cart'}>Cart (0)</Link>
+        </nav>
+      </div>
     </header>
   )
 }
