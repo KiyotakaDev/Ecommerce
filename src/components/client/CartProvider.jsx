3d81f96b4e207209ba3f86fbd08646a3df551e7b
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-12 18:54:59 -0500

    feat(actions): Adding and removing product

--- src/components/client/CartProvider.jsx
+++ src/components/client/CartProvider.jsx
@@ -12,41 +12,51 @@ export const useCartContext = () => {
 export const CartProvider = ({ children }) => {
   const defaultProducts = JSON.parse(localStorage.getItem("cart"));
   const [cartProducts, setCartProducts] = useState(defaultProducts || []);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (cartProducts.length > 0) {
+    if (cartProducts.size > 0) {
       localStorage.setItem("cart", JSON.stringify(cartProducts));
     }
   }, [cartProducts]);
 
   const getCartProducts = async () => {
     try {
-      setIsLoading(true)
+      setIsLoading(true);
       const productsResponse = await Promise.all(
         cartProducts.map(async (id) => {
-          const response = await axios.get(`/api/client/product/${id}`)
-          return response.data.products
+          const response = await axios.get(`/api/client/product/${id}`);
+          return response.data.products;
         })
-      )
-      setIsLoading(false)
-      return productsResponse
+      );
+      setIsLoading(false);
+      return productsResponse;
     } catch (error) {
-      console.log(error.response.data.error);
+      console.log(error.message);
     }
-  }
+  };
 
-  const addProductToCart = (product) => {
-    setCartProducts((prev) => [...prev, product]);
+  const addProductToCart = (productID) => {
+    setCartProducts((prev) => [...prev, productID]);
+  };
+  const removeCartProduct = (productID) => {
+    setCartProducts((prev) => {
+      const position = prev.indexOf(productID);
+      if (position !== -1) {
+        return prev.filter((_, index) => index !== position);
+      }
+      return prev;
+    });
   };
 
   return (
     <CartContext.Provider
       value={{
         cartProducts,
-        addProductToCart,
         getCartProducts,
-        isLoading
+        isLoading,
+        addProductToCart,
+        removeCartProduct,
       }}
     >
       {children}
