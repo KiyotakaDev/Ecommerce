48cd992c442b37a52a21a0bcaddc57ed70bf8ca7
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-12 15:46:14 -0500

    feat(ecommerce): Link

--- src/components/admin/Nav.jsx
+++ src/components/admin/Nav.jsx
@@ -4,8 +4,8 @@ import { navLinks } from "@/constants";
 import { HomeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ArrowLeftStartOnRectangleIcon } from '@heroicons/react/24/outline'
-import { signOut } from 'next-auth/react'
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import { signOut } from "next-auth/react";
 
 const Nav = () => {
   const path = usePathname();
@@ -18,26 +18,34 @@ const Nav = () => {
 
       <nav className="flex flex-col gap-2">
         <div>
-          <Link
-            href={"/admin"}
-            className={`navi ${
-              path === "/admin" ? "bg-violet-200 text-primary" : ""
-            }`}
-          >
-            <HomeIcon className="icon" />
-            Dashboard
-          </Link>
           {navLinks.map((link) => (
-            <Link
-              key={link.name}
-              href={link.to}
-              className={` navi ${
-                path.includes(link.to) ? "bg-violet-200 text-primary" : ""
-              }`}
-            >
-              <link.icon className={`icon ${path.includes(link.to) ? "stroke-indigo-800" : ""}`} />
-              <span>{link.name}</span>
-            </Link>
+            <>
+              <Link
+                key={link.name}
+                href={link.to}
+                className={` navi ${
+                  path.includes(link.to) ? "bg-violet-200 text-primary" : ""
+                }`}
+              >
+                <link.icon
+                  className={`icon ${
+                    path.includes(link.to) ? "stroke-indigo-800" : ""
+                  }`}
+                />
+                <span>{link.name}</span>
+              </Link>
+              {link.name === "Ecommerce" ? (
+                <Link
+                  href={"/admin"}
+                  className={`navi ${
+                    path === "/admin" ? "bg-violet-200 text-primary" : ""
+                  }`}
+                >
+                  <HomeIcon className="icon" />
+                  Dashboard
+                </Link>
+              ) : null}
+            </>
           ))}
         </div>
         <div>
