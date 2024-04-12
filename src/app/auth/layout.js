3ef322f13778f0b57c2958763f6d0e77371c32b4
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-12 15:36:08 -0500

    feat(auth): Layout

---
+++ src/app/auth/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Admin',
+  description: 'Admin authentication',
+}
+
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
