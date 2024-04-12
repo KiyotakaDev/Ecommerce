61f49d27a165f233759ae9114b286462d60597cf
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-11 19:29:40 -0500

    feat(home): Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import Featured from '@/components/client/Featured'
-import Header from '@/components/client/Header'
 import NewProducts from '@/components/client/NewProducts'
 
 const HomePage = () => {
   return (
     <div className='bg-gray-200'>
-      <Header />
       <Featured />
       <NewProducts />
     </div>
