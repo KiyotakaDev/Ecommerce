0046338678634dc793336f3b1046bb86e0551dfe
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-13 13:50:29 -0500

    feat(highlight): For hovered images

--- src/components/client/ProductImages.jsx
+++ src/components/client/ProductImages.jsx
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 
 const ProductImages = ({ images }) => {
   const [activeImage, setActiveImage] = useState(images?.[0]);
+  const [isActive, setIsActive] = useState(false);
 
   return (
-    <div>
+    <div className="h-[50vh] flex flex-col justify-between">
       <div className="flex justify-center items-center mb-4 shadow-xl rounded-lg">
-        <img src={activeImage} className="w-auto h-56" />
+        <img src={activeImage} className="w-auto h-72" />
       </div>
       <div className="grid grid-cols-3 gap-x-2">
         {images.map((image, index) => (
           <div
             key={index}
-            onMouseEnter={() => setActiveImage(image)}
-            className="preview-image rounded-lg cursor-pointer flex justify-center items-center p-2"
+            onMouseEnter={() => {
+              setActiveImage(image), setIsActive(true);
+            }}
+            onMouseLeave={() => setIsActive(false)}
+            className={`transition-all duration-200 ease-in-out preview-image rounded-lg cursor-pointer flex justify-center items-center p-2 ${
+              isActive ? "hover:-translate-y-4" : ""
+            }`}
           >
-            <img src={image} alt="prev" />
+            <img src={image} alt="prev" className="h-32 w-auto" />
           </div>
         ))}
       </div>
