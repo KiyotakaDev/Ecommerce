d188e08e800208cb91677af1e8e69d4cb7534a29
KiyotakaDev <c4mo.sw@gmail.com>
2024-04-12 22:31:59 -0500

    feat(all-products): Page

---
+++ src/app/ecommerce/products/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import ProductCard from "@/components/client/sub/ProductCard";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const AllProducts = () => {
+  const [allProducts, setAllProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getAllProducts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await axios.get("/api/client/product/all");
+        setAllProducts(response.data.products);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getAllProducts();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <div>loading</div>
+      ) : (
+        <div className="wrapper">
+          <h1 className="text-4xl text-violet-950 font-bold tracking-wide mt-10 mb-5">
+            All products
+          </h1>
+          <div className="allproducts-grid gap-10">
+            {allProducts && allProducts.map((prod, index) => (
+              <ProductCard key={index} {...prod} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AllProducts;
